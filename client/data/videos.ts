5004f102ec2a305ac4b4e99bf1ae711159114d36
Pooja Jain <pooja@email.com>
2026-07-06 03:35:09 +0530

    Update project

--- client/data/videos.ts
+++ client/data/videos.ts
@@ -8,9 +8,9 @@
 
 export const VIDEOS: Video[] = [
   // â”€â”€ Featured Originals â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
-  { id: "1",   name: "RAAZ BEYOND FEAR",          videoPath: "https://vz-012bcd01-e4e.b-cdn.net/a6835e5b-88a3-4c2a-8e9e-972a8e46588c/play_480p.mp4", category: "Featured Originals", thumbnail: "/landscape/1.png" },
-  { id: "7",   name: "RAAZ, REVENGE & MAFIA EP1", videoPath: "https://vz-012bcd01-e4e.b-cdn.net/ee0edf68-632d-4613-97cb-5dfccbcf327f/play_480p.mp4", category: "Featured Originals", thumbnail: "/landscape/7.png" },
-  { id: "8",   name: "RAAZ, REVENGE & MAFIA EP2", videoPath: "https://vz-012bcd01-e4e.b-cdn.net/6404e890-3e18-4cab-95f7-3ae76fc0aeb8/play_480p.mp4", category: "Featured Originals", thumbnail: "/landscape/8.png" },
+  { id: "1",   name: "RAAZ BEYOND FEAR",          videoPath: "https://vz-012bcd01-e4e.b-cdn.net/e28ebc9f-b16c-46ac-8c95-bb8dec174b5a/play_480p.mp4", category: "Featured Originals", thumbnail: "/landscape/1.png" },
+  { id: "7",   name: "RAAZ, REVENGE & MAFIA EP1", videoPath: "https://vz-012bcd01-e4e.b-cdn.net/8c604cec-990c-415d-8086-b62beddc8c8f/play_480p.mp4", category: "Featured Originals", thumbnail: "/landscape/7.png" },
+  { id: "8",   name: "RAAZ, REVENGE & MAFIA EP2", videoPath: "https://vz-012bcd01-e4e.b-cdn.net/a6c63b7a-2197-4614-b4c7-7580d9b7b0de/play_480p.mp4", category: "Featured Originals", thumbnail: "/landscape/8.png" },
   { id: "43",  name: "HER STORY",                 videoPath: "https://vz-012bcd01-e4e.b-cdn.net/e8ad8fc4-49fa-43ab-9fda-36916e56d066/play_480p.mp4", category: "Featured Originals", thumbnail: "/landscape/43.png" },
   { id: "16",  name: "THE FINAL SECRET",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/3ed4455a-3408-42e9-bf9a-5f45d616ad76/play_480p.mp4", category: "Featured Originals", thumbnail: "/landscape/16.png" },
   { id: "18",  name: "THE FINAL DHOKHA",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/ed0bce30-6b78-4d14-8c40-e5971b4f3b8d/play_480p.mp4", category: "Featured Originals", thumbnail: "/landscape/18.png" },
@@ -23,9 +23,9 @@ export const VIDEOS: Video[] = [
 
   // â”€â”€ Crime & Mafia â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
   { id: "11",  name: "FATAL CONNECTIONS EP1",      videoPath: "https://vz-012bcd01-e4e.b-cdn.net/2599bc17-5da5-4ebe-88c6-4e388cfb6e7a/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/11.png" },
-  { id: "12",  name: "FATAL CONNECTIONS EP2",      videoPath: "https://vz-012bcd01-e4e.b-cdn.net/fc291be4-3392-44be-aef8-d4d056051f1f/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/12.png" },
-  { id: "13",  name: "FATAL CONNECTIONS EP3",      videoPath: "https://vz-012bcd01-e4e.b-cdn.net/fa05d0b5-a766-4810-b989-a796daf5082e/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/13.png" },
-  { id: "39",  name: "WANTED FOR REVENGE",         videoPath: "https://vz-012bcd01-e4e.b-cdn.net/a1acd60e-f874-466e-a7aa-c76db9cb57df/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/39.png" },
+  { id: "12",  name: "FATAL CONNECTIONS EP2",      videoPath: "https://vz-012bcd01-e4e.b-cdn.net/95c61a9e-ae0a-43ca-8250-459fbc6b2228/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/12.png" },
+  { id: "13",  name: "FATAL CONNECTIONS EP3",      videoPath: "https://vz-012bcd01-e4e.b-cdn.net/e039a210-2833-4540-8ed2-882920df66ad/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/13.png" },
+  { id: "39",  name: "WANTED FOR REVENGE",         videoPath: "https://vz-012bcd01-e4e.b-cdn.net/8114c87c-6962-4dcc-b096-27c4c32ad78b/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/39.png" },
   { id: "53",  name: "THE WANTED TARGET",          videoPath: "https://vz-012bcd01-e4e.b-cdn.net/b9704452-fd1f-431d-a921-a0fa8187a170/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/53.png" },
   { id: "62",  name: "THE FITNESS TRAP",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/a74bbde6-2f05-49b7-88b5-68ad82b4186c/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/62.png" },
   { id: "63",  name: "THE LAST DEAL",              videoPath: "https://vz-012bcd01-e4e.b-cdn.net/2508404e-25a4-475c-8c05-af707dbf1e5d/play_480p.mp4", category: "Crime & Mafia",      thumbnail: "/portrait/63.png" },
@@ -49,7 +49,7 @@ export const VIDEOS: Video[] = [
   { id: "48",  name: "ESCAPE BEYOND FEAR EP3",     videoPath: "https://vz-012bcd01-e4e.b-cdn.net/06429499-6d65-49a3-98e6-84777884c4a2/play_480p.mp4", category: "Action & Mission",   thumbnail: "/portrait/48.png" },
   { id: "49",  name: "UNDERGROUND WARRIORS EP1",   videoPath: "https://vz-012bcd01-e4e.b-cdn.net/f7369a9f-d2e8-4c89-9cf2-87cb013dd9d0/play_480p.mp4", category: "Action & Mission",   thumbnail: "/portrait/49.png" },
   { id: "50",  name: "UNDERGROUND WARRIORS EP2",   videoPath: "https://vz-012bcd01-e4e.b-cdn.net/c91afc25-592f-4923-9ab4-62f18da47bc6/play_480p.mp4", category: "Action & Mission",   thumbnail: "/portrait/50.png" },
-  { id: "66",  name: "MISSSION DARKNIGHT",         videoPath: "https://vz-012bcd01-e4e.b-cdn.net/bf3ebf7d-ca49-443f-b7b8-1a3d4e2ae96b/play_480p.mp4", category: "Action & Mission",   thumbnail: "/portrait/66.png" },
+  { id: "66",  name: "MISSSION DARKNIGHT",         videoPath: "https://vz-012bcd01-e4e.b-cdn.net/639f02ab-e3db-434a-872a-72b38b4b277e/play_480p.mp4", category: "Action & Mission",   thumbnail: "/portrait/66.png" },
   { id: "69",  name: "ESCAPE ROUT 21",             videoPath: "https://vz-012bcd01-e4e.b-cdn.net/390332b1-be21-4c28-9bf5-06832673baad/play_480p.mp4", category: "Action & Mission",   thumbnail: "/portrait/69.png" },
   { id: "91",  name: "CHASE TO DANGER EP1",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/799c717b-cbcf-4050-8f14-2a9e199afadf/play_480p.mp4", category: "Action & Mission",   thumbnail: "/portrait/6.png" },
   { id: "92",  name: "CHASE TO DANGER EP2",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/671096db-84aa-4452-8298-d564b5fe5a41/play_480p.mp4", category: "Action & Mission",   thumbnail: "/portrait/17.png" },
@@ -57,8 +57,8 @@ export const VIDEOS: Video[] = [
   { id: "94",  name: "CHASE TO DANGER EP4",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/af763878-2990-48bd-83f1-b396dbce21f7/play_480p.mp4", category: "Action & Mission",   thumbnail: "/portrait/37.png" },
 
   // â”€â”€ Mystery & Suspense â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
-  { id: "2",   name: "THE HIDDEN TRUTH",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/803b1a1b-4b7a-4376-ae00-118c611ad37d/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/2.png" },
-  { id: "4",   name: "THE MISSING WITNESS",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/9b9b7858-2935-4a71-8554-cb6aa69eefdc/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/4.png" },
+  { id: "2",   name: "THE HIDDEN TRUTH",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/e623761c-e400-4d8d-b5f8-49d66d1f8a5b/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/2.png" },
+  { id: "4",   name: "THE MISSING WITNESS",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/e852da10-0129-4dba-8eb6-7895cc242ee6/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/4.png" },
   { id: "10",  name: "THE FORBIDDEN FILES",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/85f84324-4088-4cc8-8ea2-ca99cb7bc568/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/10.png" },
   { id: "14",  name: "THE HIDDEN ENEMY",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/47557aed-0f85-45cf-a818-19fd88a2b8de/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/14.png" },
   { id: "22",  name: "THE MISSING LINK",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/11444673-b5cf-43a1-a6fa-7cc8e4552e96/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/22.png" },
@@ -76,10 +76,10 @@ export const VIDEOS: Video[] = [
   { id: "78",  name: "THE LAST CHANCE",            videoPath: "https://vz-012bcd01-e4e.b-cdn.net/20428f28-cc91-4463-bb07-df334be38ab3/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/78.png" },
   { id: "81",  name: "MYSTERY AVENUE",             videoPath: "https://vz-012bcd01-e4e.b-cdn.net/06572887-18a3-4cb0-bc48-59323e881c35/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/81.png" },
   { id: "89",  name: "THE UNOFFICIAL NETWORK",     videoPath: "https://vz-012bcd01-e4e.b-cdn.net/a90c6024-768c-4c24-87d4-07612b07477a/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/89.png" },
-  { id: "102", name: "MIDNIGHT CASE",              videoPath: "https://vz-012bcd01-e4e.b-cdn.net/0ec9da21-5da0-42b1-a81a-2eb1ee0c98e5/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/102.png" },
+  { id: "102", name: "MIDNIGHT CASE",              videoPath: "https://vz-012bcd01-e4e.b-cdn.net/f72bac3d-f5f6-4104-9a5d-dd37062a81c4/play_480p.mp4", category: "Mystery & Suspense", thumbnail: "/landscape/102.png" },
 
   // â”€â”€ Dark Thrillers â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
-  { id: "3",   name: "SCILENT CHASE",              videoPath: "https://vz-012bcd01-e4e.b-cdn.net/e7654d3b-fef4-4867-8426-e8deabada55b/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/3.png" },
+  { id: "3",   name: "SCILENT CHASE",              videoPath: "https://vz-012bcd01-e4e.b-cdn.net/5baa9dae-1cda-4312-a386-2eae75c6519f/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/3.png" },
   { id: "9",   name: "SCILENT TRIGGER",            videoPath: "https://vz-012bcd01-e4e.b-cdn.net/204461ac-ae3c-4d31-a393-ed37c9a2ae91/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/9.png" },
   { id: "19",  name: "BLACK DIARY SECRETS EP1",    videoPath: "https://vz-012bcd01-e4e.b-cdn.net/62b9d1e4-4a58-440d-aced-b23367687127/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/19.png" },
   { id: "20",  name: "BLACK DIARY SECRETS EP2",    videoPath: "https://vz-012bcd01-e4e.b-cdn.net/34f1001c-5f45-4a80-8a76-a2bee40bb09a/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/20.png" },
@@ -87,7 +87,7 @@ export const VIDEOS: Video[] = [
   { id: "29",  name: "DANGEROUS MINDS EP2",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/ac231f13-5e7b-40fb-af8e-97be64977c56/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/29.png" },
   { id: "30",  name: "DANGEROUS MINDS EP3",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/55c72f41-bd2a-4a41-b9a2-a171aefd7015/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/30.png" },
   { id: "31",  name: "DANGEROUS MINDS EP4",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/fb60c689-0ae6-4d37-9e7a-0a6fd490aef0/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/31.png" },
-  { id: "35",  name: "THE DARK NETWORK",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/e9657c34-99bf-4067-bb06-6c1fae913f0c/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/35.png" },
+  { id: "35",  name: "THE DARK NETWORK",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/c729a39a-a052-4f10-a6ab-5cb24881e727/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/35.png" },
   { id: "44",  name: "DANGEROUS TERRITORY",        videoPath: "https://vz-012bcd01-e4e.b-cdn.net/ee836cdd-1c34-4b4d-843c-3d2c4be8e879/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/44.png" },
   { id: "45",  name: "SHADOW PROTOCOL",            videoPath: "https://vz-012bcd01-e4e.b-cdn.net/08ebb0ad-11cd-458a-823b-3e19382347aa/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/45.png" },
   { id: "52",  name: "DARK CITY FILES",            videoPath: "https://vz-012bcd01-e4e.b-cdn.net/64ef26c1-c28b-4cd4-926e-7f878f62e2c1/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/2.png" },
@@ -108,14 +108,14 @@ export const VIDEOS: Video[] = [
   { id: "100", name: "DARK EMPIRE",                videoPath: "https://vz-012bcd01-e4e.b-cdn.net/2c8a4f59-3274-4347-ac68-5ff4d0789dae/play_480p.mp4", category: "Dark Thrillers",     thumbnail: "/portrait/8.png" },
 
   // â”€â”€ Secrets & Betrayal â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€â”€
-  { id: "5",   name: "THE SECRET ROUT EP 1",       videoPath: "https://vz-012bcd01-e4e.b-cdn.net/3ff5f869-4419-444b-a338-870703a42f6c/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/5.png" },
-  { id: "6",   name: "THE SECRET ROUT EP 2",       videoPath: "https://vz-012bcd01-e4e.b-cdn.net/e0608c54-d8e5-4bfb-b354-4ee545847c3e/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/6.png" },
+  { id: "5",   name: "THE SECRET ROUT EP 1",       videoPath: "https://vz-012bcd01-e4e.b-cdn.net/bfbdaf66-9be8-48f1-9152-d35b5d3e2c2f/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/5.png" },
+  { id: "6",   name: "THE SECRET ROUT EP 2",       videoPath: "https://vz-012bcd01-e4e.b-cdn.net/d2e7baf8-81a6-4092-8432-ec006b17ec32/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/6.png" },
   { id: "17",  name: "THE SECRET ORDER",           videoPath: "https://vz-012bcd01-e4e.b-cdn.net/d6698497-d9a5-49ca-aae7-a937f0a563bc/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/17.png" },
   { id: "24",  name: "DANGEROUS ALLIANCE",         videoPath: "https://vz-012bcd01-e4e.b-cdn.net/84b04739-d8f4-4a60-9a0c-1956fc0c562d/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/24.png" },
-  { id: "36",  name: "THE SECRET MISSION",         videoPath: "https://vz-012bcd01-e4e.b-cdn.net/4cb2c276-8ecd-49ef-967c-48ba4631aa1b/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/36.png" },
+  { id: "36",  name: "THE SECRET MISSION",         videoPath: "https://vz-012bcd01-e4e.b-cdn.net/5afa0c99-8c48-4c37-a783-6c3cee7a10bf/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/36.png" },
   { id: "37",  name: "THE SECRET SYNDICATE",       videoPath: "https://vz-012bcd01-e4e.b-cdn.net/6cffca6b-7759-40e7-84de-c97856b4c7be/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/37.png" },
   { id: "42",  name: "THE DIARY SECRETS",          videoPath: "https://vz-012bcd01-e4e.b-cdn.net/d1f6716d-b7b1-41c3-8e4a-fd7204a75a74/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/42.png" },
-  { id: "65",  name: "SECRET NIGHTS",              videoPath: "https://vz-012bcd01-e4e.b-cdn.net/0091603b-6880-49c6-ac89-efab0ec04bb8/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/65.png" },
+  { id: "65",  name: "SECRET NIGHTS",              videoPath: "https://vz-012bcd01-e4e.b-cdn.net/d3901ece-5255-4604-b14c-085219a981b3/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/65.png" },
   { id: "82",  name: "DANGEROUS DESTINATION EP1",  videoPath: "https://vz-012bcd01-e4e.b-cdn.net/6e1962ba-dbcc-4c2b-963a-d3176124deb4/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/82.png" },
   { id: "83",  name: "DANGEROUS DESTINATION EP2",  videoPath: "https://vz-012bcd01-e4e.b-cdn.net/4527946b-8e26-4e53-87da-ab74f2313614/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/83.png" },
   { id: "84",  name: "DANGEROUS DESTINATION EP3",  videoPath: "https://vz-012bcd01-e4e.b-cdn.net/613259cb-60be-4296-96ca-63ebc21922c0/play_480p.mp4", category: "Secrets & Betrayal", thumbnail: "/landscape/84.png" },
